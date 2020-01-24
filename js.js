50d236be047d5c8e36a9c9cdacf9318e57666a61
Anna Korotkova <ania.korotkovafd@gmail.com>
2020-01-24 11:51:23 +0200

    DZ3

--- js.js
+++ js.js
@@ -1,107 +1,53 @@
-class Car {
+'use strict';
 
-    _engineСapacity = [];
+document.addEventListener("DOMContentLoaded", () => {
 
-    constructor(props) {
-        this._owner = props.name
+    // First
+
+    let app = document.getElementById('app');
+
+    let bCreate = document.getElementById('create'),
+        bRead = document.getElementById('read'),
+        bUpdate = document.getElementById('update'),
+        bDelete = document.getElementById('delete');
+
+    bCreate.addEventListener('click', createTitle);
+    bRead.addEventListener('click', read);
+    bUpdate.addEventListener('click', update);
+    bDelete.addEventListener('click', deleteTitle);
+
+    function createTitle() {
+        let title = prompt('Enter title');
+
+        if (title) {
+            let el = document.createElement('h1');
+            el.id = 'title'
+            el.innerHTML = title;
+            app.append(el);
+        }
     }
 
-    get owner() {
-        return this._owner
+    function read() {
+        console.log(app.children.title.innerHTML);
     }
 
-    set owner(name) {
-        this._owner = name
+    function update() {
+        let newTitle = prompt('Enter new rirle', app.children.title.innerHTML);
+        app.children.title.innerHTML = newTitle;
+
     }
 
-    set engineСapacity(val){
-        this._engineСapacity.push(val)
+    function  deleteTitle () {
+        let confirmM = confirm('Are you sure?');
+        if (confirmM) {
+            app.removeChild(title)
+        }
     }
 
-    get engineСapacity(){
-        return this._engineСapacity.join(' ');
-    }
+    // Second
 
-}
+    let list = document.getElementById('items-list');
+    console.log(list.firstChild.nextSibling);
+    console.log(list.firstChild.parentNode);
 
-class GermanyCar extends Car {
-
-    constructor(props) {
-        super(props);
-        this.country = "Germany";
-    }
-
-    startCar(){
-        console.log('Your GermanyCar car was started')
-    }
-
-}
-
-class Audi extends GermanyCar {
-
-    constructor(props) {
-        super(props);
-        this.model = props.model;
-    }
-
-    startCar(){
-        console.log('Your Audi car was started')
-    }
-    
-    visitAudiServece(){
-        return "You've been arrived to Audi service center"
-    }
-
-}
-
-class AudiA4 extends Audi {
-
-    #pass = '123456';
-
-    constructor(props) {
-        super(props);
-        this.color = props.color;
-        this._year = props.year
-        this._vinCode = function () { return props.vinCode }
-    }
-
-    static getPass(){
-        return 'Call static method'
-    }
-
-    get vinCode(){
-        return this._vinCode();
-    }
-
-    get year() {
-        return this._year
-    }
-
-
-    incrementYear() {
-        this._year++
-    }
-
-    decrementYear() {
-        this._year--
-    }
-}
-
-console.log(AudiA4.getPass());
-
-let carOps = {
-    name: 'anna',
-    model: 'a4',
-    color: "white",
-    year: 2011,
-    vinCode: 'D4545N128'
-}
-
-const car1 = new AudiA4(carOps);
-const car2 = Object.assign(car1);
-
-
-
-
-
-
+})
